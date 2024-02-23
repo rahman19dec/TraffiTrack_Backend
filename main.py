1a6d0ec3e0df698beb8d60eef6b87208054b71c1
bad fad <badfad@edu.novia.fi>
2024-02-23 18:14:01 +0200

    fixed -db, db test

--- main.py
+++ main.py
@@ -1,63 +1,26 @@
-# Creted by: Ahmad Imam (ahmadimam657@gmail.com)
-
-
-
 from ultralytics import YOLO
 import cv2
 import numpy as np
 import sqlite3
-import ast
 from datetime import datetime
 import matplotlib.pyplot as plt
 from collections import defaultdict
-from utils import *
-
-
+from main_utils import *
 
 MODEL = "best.pt"
 model = YOLO(MODEL)
 
-# Open the video file
-video_path = "test_video.mp4"
-video_name_track = 'track_result.avi'
-cap = cv2.VideoCapture(video_path)
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-video_name_track = cv2.VideoWriter(video_name_track, 0, 30, (frame_width,frame_height))
-
-
-
-
-heatmap_video_writer = cv2.VideoWriter('heatmap_result.avi', 0, 30, (frame_width,frame_height))
 heatmap_obj = heatmap.Heatmap()
 heatmap_obj.set_args(colormap=cv2.COLORMAP_PARULA, imw=frame_width, imh=frame_height, shape="circle")
-
-
 
 detections = []
 count = 0
 track_history = defaultdict(lambda: [])
-while cap.isOpened():
-  success,image = cap.read()
-  if success:
-    results = model.track(image, persist=True,tracker="bytetrack.yaml")
-    heatmap_frame = heatmap_obj.generate_heatmap(image.copy(), results)
-    heatmap_video_writer.write(heatmap_frame)
-    if results[0].boxes.id != None:
-      detections.append({'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),'xyxy':list(results[0].boxes.xyxy.cpu().tolist()),
-      'confidence':list(results[0].boxes.conf.cpu().tolist()),
-      'class_id':list(results[0].boxes.cls.cpu().int().tolist()),
-      'object_id':list(map(int,list(results[0].boxes.id.cpu().tolist())))})
-      frame = results[0].plot()
-      annotated_frame = plot_tracks(frame, results, track_history)
-    else:
-      annotated_frame = None
-    if annotated_frame is not None and annotated_frame.size > 0:
-      video_name_track.write(annotated_frame)
-    else:
-      video_name_track.write(image)
-  else:
-    break
 
 conn = sqlite3.connect('predictions_database.db')
 cursor = conn.cursor()
@@ -65,28 +28,55 @@ cursor = conn.cursor()
 # Create predictions table if it does not exit
 create_predictions_table(cursor)
 
-insert_predictions(cursor, detections)
+# Function to update the Matplotlib plot with new frames
+def update_plot(frame):
+    plt.clf()
+    plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    plt.axis('off')
+    plt.pause(0.1)
 
+plt.ion()  # Turn on interactive mode for Matplotlib
+#[{'time': '2024-02-23 14:20:37', 'xyxy': [[28.64773178100586, 134.34608459472656, 640.0, 479.8232727050781]], 'confidence': [0.893378496170044], 'class_id': [0], 'object_id': [1]}]
+try:
+    while cap.isOpened():
+        success, image = cap.read()
+        if success:
+            results = model.track(image, persist=True, tracker="bytetrack.yaml")
+            heatmap_frame = heatmap_obj.generate_heatmap(image.copy(), results)
+            update_plot(heatmap_frame)
+            if results[0].boxes.id is not None:
+                detections = [{'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                               'xyxy': list(results[0].boxes.xyxy.cpu().tolist()),
+                               'confidence': list(results[0].boxes.conf.cpu().tolist()),
+                               'class_id': list(results[0].boxes.cls.cpu().int().tolist()),
+                               'object_id': list(map(int, list(results[0].boxes.id.cpu().tolist())))}]
+                frame = results[0].plot()
+                annotated_frame = plot_tracks(frame, results, track_history)
+                try:
+                    insert_predictions(cursor, detections)
+                    conn.commit()  # Commit the transaction
+                except Exception as e:
+                    print("Error inserting data into database:", e)
+                    conn.rollback()  # Rollback the transaction in case of an error
+            else:
+                annotated_frame = None
 
-cursor.execute('SELECT * FROM predictions_bytime')
-rows = cursor.fetchall()
+            if annotated_frame is not None and annotated_frame.size > 0:
+              
+                # update_plot(annotated_frame)
+                pass
+            else:
+              pass
+                # update_plot(image)
 
+            plt.pause(0.5)
 
+        else:
+            break
 
-count_objects,total_objects,time_objects = get_object_counts(model.names,cursor)
+except KeyboardInterrupt:
+    pass
+  
 
-
-print(f"Total objects detected: {total_objects}")
-print(f"type wise count:", {model.names[k]:len(v) for k,v in count_objects.items()})
-
-
-times = list(time_objects.keys())
-counts = list(time_objects.values())
-
-# plot_filtered_counts_over_time(times, counts)
-plot_filtered_counts_over_time(times, counts)
-plot_non_zero_object_distribution(model.names,count_objects)
-
-
-dt = [i['xyxy'] for i in detections]
-create_heatmap(cv2.imread('frame0.jpg'),dt)
+cap.release()
+cv2.destroyAllWindows()
