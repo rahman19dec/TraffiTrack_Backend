28f679a4a5fc3c3083caf32f532b01f53964b013
rahman19dec <muzahid19dec@hotmail.com>
2024-03-02 01:28:58 +0200

    rearrangingAppPy

--- fapi/app.py
+++ fapi/app.py
@@ -9,7 +9,9 @@ classes = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorcycle',
             4: 'bus', 5: 'truck'}
 
 
-
+@app.route('/classes', methods=['GET'])
+def get_classes():
+    return jsonify(classes), 500
 
 
 def read_db(command):
@@ -40,8 +42,51 @@ def get_all_data():
     command = 'SELECT * FROM detection_bytime'
     return jsonify(read_db(command))
 
+
 @app.route('/count', methods=['GET'])
 def get_count():
+    # Get optional parameters from the request
+    # example: http://127.0.0.1:5000/count?from_time=2024-02-24%2015:00:00&to_time=2025-02-25%2016:00:00
+    from_time = request.args.get('from_time')
+    to_time = request.args.get('to_time')
+    print(from_time, to_time)
+    
+    # Construct the base SQL command
+    command = 'SELECT * FROM detection_bytime'
+    
+    # Add WHERE clause to filter by time range if both from_time and to_time are provided
+    if from_time and to_time:
+        command += f" WHERE time BETWEEN '{from_time.replace('T',' ')}' AND '{to_time.replace('T',' ')}'"
+    elif from_time:
+        command += f" WHERE time >= '{from_time.replace('T',' ')}'"
+    elif to_time:
+        command += f" WHERE time <= '{to_time.replace('T',' ')}'"
+    
+    # Execute the database query
+    data = read_db(command)
+    
+    # Process the data to calculate counts
+    count = {i: 0 for i, _ in classes.items()}
+    last_count = count.copy()
+
+    for arr in data:
+        rec_count = {element: c for element, c in Counter(ast.literal_eval(arr[4])).items()}
+        rec_count = {i: rec_count.get(i, 0) for i in range (6)}
+
+        for k, v in rec_count.items():
+            if last_count[k] < v:
+                count[k] += v - last_count[k]
+
+        print(rec_count, last_count, count)
+        last_count.update(rec_count)
+
+
+    # Return the counts as JSON
+    return jsonify(count)
+
+
+@app.route('/stat', methods=['GET'])
+def get_stat():
     # Get optional parameters from the request
     # example: http://127.0.0.1:5000/count?from_time=2024-02-24%2015:00:00&to_time=2025-02-25%2016:00:00
     from_time = request.args.get('from_time')
@@ -86,9 +131,6 @@ def get_count():
 
 
 
-@app.route('/classes', methods=['GET'])
-def get_classes():
-    return jsonify(classes), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
